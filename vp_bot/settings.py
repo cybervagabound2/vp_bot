d443096041180ba9b51e6d3d1d93b2a8e658be7a
cybervagabound <40501099+cybervagabound2@users.noreply.github.com>
2018-11-15 20:58:17 -0500

    Update settings.py

--- vp_bot/settings.py
+++ vp_bot/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gy@#3!h7k5pukc6^#&(=xv@rcd5zq@u-7b6zb6z#a!yjy#%5!@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
